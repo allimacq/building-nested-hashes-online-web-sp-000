086e4bbbecd0cd0f60835600f18bd42e9f3328b3
Allison MacQueen <allimacq@gmail.com>
2020-09-09 19:30:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -35,6 +35,23 @@ def first_challenge
            :name => "Lady Capulet",
            :age => "51"
          },
+         :heroine => {
+           :name => "Juliet",
+           :age => "15",
+           :status => "alive"
+         },
+         :heroine_friends => [
+           {
+             :name => "Steven",
+             :age => "30",
+             :attitude => "confused"
+           },
+           {
+             :name => "Nurse",
+             :age => "44",
+             :attitude => "worried"
+             }
+            ]
        }
        
      }
