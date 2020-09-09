29c2fac5d2644605a6148a6d2d677c303201b00d
Allison MacQueen <allimacq@gmail.com>
2020-09-09 19:27:57 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,7 +7,36 @@ def first_challenge
        },
        :matriarch => {
          :name => "Lady Montague",
+         :age => "54"
+       },
+       :hero => {
+         :name => "Romeo",
+         :age => "15"
+       },
+       :hero_friends => [
+         {
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
+         },
+         {
+           :name => "Mercutio"
+           :age => "18",
+           :attitude => "hot-headed"
+         }
+        ]
+       },
+       :capulet => {
+         :patriach => {
+           :name => "Lord Capulet",
+           :age => "50"
+         },
+         :matriarch => {
+           :name => "Lady Capulet",
+           :age => "51"
+         },
        }
+       
      }
    }
 end
