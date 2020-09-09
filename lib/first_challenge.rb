8140d172b77a4bbfea0b88876fe0fe255b713cbc
Allison MacQueen <allimacq@gmail.com>
2020-09-09 19:32:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,13 +5,13 @@ def first_challenge
          :name => "Lord Montague",
          :age => "53"
        },
-       :matriarch => {
-         :name => "Lady Montague",
-         :age => "54"
+        :matriarch => {
+          :name => "Lady Montague",
+          :age => "54"
        },
-       :hero => {
-         :name => "Romeo",
-         :age => "15"
+        :hero => {
+          :name => "Romeo",
+          :age => "15"
        },
        :hero_friends => [
          {
